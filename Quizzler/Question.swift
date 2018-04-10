928988fe0c67f20dddde1e98beca82f3ac307db9
Tofique Ahmed Khan <tofiqueahmedkhan17@gmail.com>
2018-04-10 10:24:40 +0530

    Initial Commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Tofique Ahmed Khan
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+}
